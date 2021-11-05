cc1e3fb1adba50c82e661ccbe04ed7680e04c8db
Elliott Spencer <elliott.spencer1315@gmail.com>
2021-11-04 23:05:24 -0700

    removing unneeded files

--- src/index.js
+++ src/index.js
@@ -2,16 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
+// Reactdom is a method used to render a react element into the DOM
+// The first argument is the component rendered, and the second is the container element within the page
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
