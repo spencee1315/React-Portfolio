6facc01f31953c1e641cc5f8003239a0e05d00cd
Elliott Spencer <elliott.spencer1315@gmail.com>
2021-11-07 11:17:46 -0800

    npm installed react-router-dom, adjusted app.js structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,60 @@
 import React from 'react';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, NavLink } from 'react-router-dom';
+
 import './App.css';
 
 // importing components
-import Navbar from './components/Navbar';
-import Header from './components/Header';
-import MainSection from './components/MainSection';
-import Projects from './components/Projects';
-import Footer from './components/Footer';
-import Social from './components/Social';
+import Navbar from './components/features/Navbar';
+import Header from './components/features/Header';
+import Main from './components/main-page/Main';
+import Projects from './components/projects/Projects';
+import Footer from './components/features/Footer';
+import Social from './components/features/Social';
 
-function App() {
+// resources used for Router, Switch, NavLink operations below: 
+// https://www.freecodecamp.org/news/react-router-cheatsheet/
+// https://www.freecodecamp.org/news/a-complete-beginners-guide-to-react-router-include-router-hooks/
+// https://v5.reactrouter.com/web/api/Switch
+// https://stackoverflow.com/questions/56764690/what-is-the-difference-between-browserrouter-and-router-in-react-router
+
+
+const App = () => {
   return (
-    <div className="bucket-app">
-      <BucketList />
-    </div>
+    <Router>
+      <div id="showcase">
+        <Navbar />
+        <Header />
+      </div>
+      <Switch>
+        <Route component={Main} path='/' exact />
+        <Route component={Projects} path='/projects' exact />
+      </Switch>
+      <div id="showcase">
+        <Footer />
+        <Social />
+      </div>
+    </Router>
   );
 }
 
+// const Navbar = () => {
+//   return (
+//     <nav>
+//       <NavLink
+//         activeStyle={{
+//           fontWeight: "bold",
+//           color: "red"
+//         }}
+//         to="/"
+//       >
+//         Main
+//       </NavLink>
+//       <NavLink activeClassName="active" to="/projects">
+//         Projects
+//       </NavLink>
+//     </nav>
+//   );
+// }
+
 export default App;
+// export default Navbar;
