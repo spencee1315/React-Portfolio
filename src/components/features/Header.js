fa4e7d31031078f7661f336da0b1e4ce8e970be0
Elliott Spencer <elliott.spencer1315@gmail.com>
2021-11-07 14:37:24 -0800

    completed Header.js structure

---
+++ src/components/features/Header.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Header() {
+    return (
+        <main className="container background-trans-50">
+           <h1>
+               Elliott Spencer's Portfolio
+           </h1>
+           <h2>
+               A Full Stack Web Developer
+           </h2>
+           <p className="lead hide-on-small">
+               Welcome to my portfolio page. Below are a series of projects as I dive deeper into this amazing subject.
+           </p>
+        </main>
+    )
+}
