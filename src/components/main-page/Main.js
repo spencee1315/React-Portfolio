d4897fe046b3fdc602d1398c2f3967b24c67a3f7
Elliott Spencer <elliott.spencer1315@gmail.com>
2021-11-07 11:22:05 -0800

    updated main page, imported react components, added page structure

---
+++ src/components/main-page/Main.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react'
+import AboutMe from './AboutMe';
+
+function Main() {
+    return (
+        <Fragment>
+            <AboutMe />
+        </Fragment>
+    )
+}
+
+export default Main;
