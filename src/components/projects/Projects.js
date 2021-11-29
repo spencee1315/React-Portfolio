cfb6cf8761e0a800c7afd0311a89287080f44893
Elliott Spencer <elliott.spencer1315@gmail.com>
2021-11-28 16:47:24 -0800

    added images, update project information

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -2,48 +2,48 @@ import React from 'react'
 import { ProjectCard } from './ProjectCard'
 
 // Importing Images
-import project1 from '../images/project1.jpg'
-import project2 from '../images/project2.jpg'
-import project3 from '../images/project3.jpg'
-import project4 from '../images/project4.jpg'
+import fitnessTrackerImg from '../../images/fitnessTracker.png'
+import echoChamberImg from '../../images/echoChamber.png'
+import noteTakerImg from '../../images/noteTaker.png'
+import weatherAppImg from '../../images/weatherApp.png'
 
 // array of project information
 export const Project = () => {
 
     const Data = [
         {
-            name: 'proj1',
-            projimg: 'img for proj1',
-            desc: 'desc for proj1',
-            url: 'github url for proj1',
-            gitrepo: 'gitrepo for proj1',
+            name: 'Fitness Tracer',
+            projimg: fitnessTrackerImg,
+            desc: 'This application is a fitness tracker that allows a user to create and track daily workouts. A user can log multiple exercises in a workout on a given day. The app has the ability to track the name, type, weight, sets, reps, and duration of an exercise, and there is an option for distance traveled should the exercise be cardio.',
+            url: 'https://fitnesstracker-uwcoding.herokuapp.com/',
+            gitrepo: 'https://github.com/spencee1315/Fitness_Tracker',
             alt: "",
             id: 1,
         },
         {
-            name: 'proj2',
-            projimg: 'img for proj2',
-            desc: 'desc for proj2',
-            url: 'github url for proj2',
-            gitrepo: 'gitrepo for proj2',
+            name: 'Echo Chamber',
+            projimg: echoChamberImg,
+            desc: 'A social media platform that uses node, express, mysql, sequelize, and handlebars as the primary tools to connect users in an anonymous form. Users can create posts and then vote on posts to decide what makes it to the top of the feed. Users will have their own profiles to review their post history and can also comment on posts.',
+            url: 'https://echochamber12.herokuapp.com/',
+            gitrepo: 'https://github.com/ZakkFast/EchoChamber',
             alt: "",
             id: 2,
         },
         {
-            name: 'proj3',
-            projimg: 'img for proj3',
-            desc: 'desc for proj3',
-            url: 'github url for proj3',
-            gitrepo: 'gitrepo for proj3',
+            name: 'Note Taker',
+            projimg: noteTakerImg,
+            desc: 'This application uses an Express.js back-end and it saves and retrieves note data from a JSON file.',
+            url: 'https://hw11-notetaker-uwbootcamp.herokuapp.com/notes',
+            gitrepo: 'https://github.com/spencee1315/hw_wk11',
             alt: "",
             id: 3,
         },
         {
-            name: 'proj4',
-            projimg: 'img for proj4',
-            desc: 'desc for proj4',
-            url: 'github url for proj4',
-            gitrepo: 'gitrepo for proj4',
+            name: 'Weather App',
+            projimg: weatherAppImg,
+            desc: 'This application is a weather dashboard that uses a third-party API to access specific weather data and functionality by making requests with specific parameters to a URL. It runs in the browser and features dynamically updated HTML, CSS and Javascript.',
+            url: 'https://spencee1315.github.io/hw_wk6/',
+            gitrepo: 'ghttps://github.com/spencee1315/hw_wk6',
             alt: "",
             id: 4,
         },
